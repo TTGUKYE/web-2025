db74351fbc0e004ac5b013383f7faf3a12f0bba7
TTGUKYE <ttpaimeps@gmail.com>
2025-03-05 09:59:18 +0300

    lab3

---
+++ asd/name.pas
@@ -0,0 +1,19 @@
+PROGRAM Greeting(INPUT, OUTPUT);
+USES
+  DOS;
+VAR
+  QueryString, Name: STRING;
+  PosName: INTEGER;
+BEGIN {Greeting}
+  WRITELN('Content-Type: text/plain');
+  WRITELN; // Пустая строка между заголовком и телом ответа
+  QueryString := GetEnv('QUERY_STRING');
+  PosName := Pos('name=', QueryString);
+  IF PosName > 0 THEN
+  BEGIN
+    Name := Copy(QueryString, PosName + Length('name='), Length(QueryString));
+    WRITELN('Hello dear, ', Name, '!')
+  END
+  ELSE
+    WRITELN('Hello Anonymous!')
+END. {Greeting}
