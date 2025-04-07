4b9b8fc7fbf17c3c08c9a5258992057243921b12
TTGUKYE <ttpaimeps@gmail.com>
2025-04-07 22:07:30 +0300

    .

--- lab7/prifile/validation.php
+++ lab7/prifile/validation.php
@@ -5,17 +5,14 @@ function validateUserData($user)
 {
     $errors = [];
 
-    // Проверка имени
     if (strlen($user["name"] ?? "") < 3) {
         $errors[] = "Имя должно содержать минимум 3 символа.";
     }
 
-    // Проверка описания
     if (strlen($user["bio"] ?? "") > 200) {
         $errors[] = "Описание не должно превышать 200 символов.";
     }
 
-    // Проверка ID
     if (!is_numeric($user["id"] ?? "")) {
         $errors[] = "ID должен быть числом.";
     }
