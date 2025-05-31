e6804cff90f645c52ef74c8f8cb99db7473fa6ac
TTGUKYE <ttpaimeps@gmail.com>
2025-05-31 09:53:48 +0300

    .

--- lab8/home/db.php
+++ lab8/home/db.php
@@ -12,9 +12,9 @@ function connectDatabase()
             $username,
             $password,
             [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //При ошибках PDO будет выбрасывать исключения (удобно для отладки).
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //Результаты запросов возвращаются в виде ассоциативных массивов (например, $row['id'])
+                PDO::ATTR_EMULATE_PREPARES => false, //Отключает эмуляцию подготовленных запросов, что повышает безопасность.
             ]
         );
         return $pdo;
